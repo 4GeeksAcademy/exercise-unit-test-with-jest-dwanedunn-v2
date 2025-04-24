f1ce50fb688d6d878e937bd25cdce9675356638d
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-24 18:13:14 +0000

    refactor functions and exports

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 
+// Function to convert Euro to Dollar
+const fromEuroToDollar = function(valueInEuro){
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+    }
+
 // Function to sum two numbers
 const sum = (a,b) => {
 return a+b
 }
 
-// Function to convert Euro to Dollar
-const fromEuroToDollar = function(valueInEuro){
-// Convert the given valueInEuro to dollars
-let valueInDollar = valueInEuro * 1.07;
-// return the dollar value
-return valueInDollar;
-}
+
 
 //console.log(sum(7,3))
 //We need to export the functions to use them.
-module.exports = {sum,fromEuroToDollafromEuroToDollar};
+module.exports = { sum, fromEuroToDollar }
